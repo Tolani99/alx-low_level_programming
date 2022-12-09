2acb6a160b4ceafdea6fa496172bcf713f07fcad
Adisa Tolani Adekunle <tolaniadisa999@gmail.com>
2022-12-09 15:03:54 +0100

    Task 7: Writes a program that prints the lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Return: 0 (Always success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
