951855eb1d36c2f841028df93d73391ae62e6e20
Adisa Tolani Adekunle <tolaniadisa999@gmail.com>
2022-12-09 03:44:00 +0100

    Task 5: Writes a C program that prints exactly 'with proper grammer, but the outcome is a piece of art'

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a function that prints exactly some word
+ *
+ * Return: 0(Always success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
