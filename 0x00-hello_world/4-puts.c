0808b15d384759ccce5e9324d0bcb6649514f3f0
Adisa Tolani Adekunle <tolaniadisa999@gmail.com>
2022-12-09 03:33:20 +0100

    Task 4: Writes a C program that prints exactly 'Programming is like building a multilingual puzzle, followed by a newline'

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - shows the body of the program
+ *
+ * Return: 0 (Always success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
