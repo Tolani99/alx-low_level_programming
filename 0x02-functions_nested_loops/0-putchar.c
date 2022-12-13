51fd562cf9212bae019070012826a93e7655a9f4
Adisa Tolani Adekunle <tolaniadisa999@gmail.com>
2022-12-13 20:57:55 +0100

    Task 0: prints _putchar followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,15 @@
  */
 int main(void)
 {
-	printf("_putchar");
-	printf("\n");
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar('\n');
 
 	return (0);
 }
