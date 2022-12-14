404ec5b0cb4f7f65b18849cebd54a4dd7d5aff39
Adisa Tolani Adekunle <tolaniadisa999@gmail.com>
2022-12-14 22:33:37 +0100

    Task 5: prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar(43);
-		putchar(1);
+		putchar('1');
 	}
 	else if (n == 0)
 	{
 		putchar(48);
-		putchar(0);
+		putchar('0');
 	}
 	else
 	{
 		putchar(45);
-		putchar(-1);
+		putchar('-');
 	}
 
 	return (0);
