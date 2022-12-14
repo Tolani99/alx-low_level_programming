aa0f542f025f31b5aa23ba1cebcfe36b91c7e84c
Adisa Tolani Adekunle <tolaniadisa999@gmail.com>
2022-12-14 22:02:56 +0100

    Task 4: checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: is the parameter used
+ *
+ * Return: 0 (Always success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
