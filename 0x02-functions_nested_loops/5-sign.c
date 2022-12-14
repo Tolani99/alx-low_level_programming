9677e1d88061b7a9a3f476ca3cdc7fa56371ec01
Adisa Tolani Adekunle <tolaniadisa999@gmail.com>
2022-12-14 22:31:37 +0100

    Task 5: prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - prints the sign of a number
- *
+ * @n: is the parameter taken in
  *
  * Return: 0 (Always success)
  */
@@ -9,17 +9,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
-		return (1);
+		putchar(43);
+		putchar(1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
-		return (0);
+		putchar(48);
+		putchar(0);
 	}
 	else
 	{
-		putchar('-');
-		return (-1);
+		putchar(45);
+		putchar(-1);
 	}
+
+	return (0);
 }
