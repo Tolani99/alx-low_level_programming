6b5f2d382a10000420e783648cd4abfafdeb1ff4
Adisa Tolani Adekunle <tolaniadisa999@gmail.com>
2022-12-14 20:34:49 +0100

    Task 0: prints _putcahr followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - prints _putchar followed by a new line
  *
  *
  * Return: 0 (Always success)
  */
-int main(void)
+int main (void)
 {
-	putchar(95);
-	putchar(112);
-	putchar(117);
-	putchar(116);
-	putchar(99);
-	putchar(104);
-	putchar(97);
-	putchar(114);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
 	putchar('\n');
 
 	return (0);
