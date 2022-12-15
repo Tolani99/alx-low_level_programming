900952b4b5dc72c72fdd0b732397c5f3b086255d
Adisa Tolani Adekunle <tolaniadisa999@gmail.com>
2022-12-15 22:28:33 +0100

    Task 3: prints the number from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * print_numbers - prints the numbers from 0 to 9
- * 
+ *
  *
  * Return: 0 (Always success)
  */
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
