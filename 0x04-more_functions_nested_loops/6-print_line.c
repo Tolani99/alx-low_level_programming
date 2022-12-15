cf19bedca3a97bf758766102af595e0acb36919f
Adisa Tolani Adekunle <tolaniadisa999@gmail.com>
2022-12-15 23:09:16 +0100

    Task 6: draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -17,16 +17,3 @@ void print_line(int n)
 		putchar('\n');
 	}
 }
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_line(0);
-    print_line(2);
-    print_line(10);
-    print_line(-4);
-    return (0);
-}
