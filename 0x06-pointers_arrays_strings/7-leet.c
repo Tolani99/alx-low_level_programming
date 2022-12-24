74b48915f8f0c3215f7a9cd2ee6849f0c4ccc99d
Adisa Tolani Adekunle <tolaniadisa999@gmail.com>
2022-12-24 05:58:10 +0100

    Task 0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,18 +1,28 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code for
- *
- * Return: Always 0.
+ * leet - encodes a string into 1337
+ * @c: String
+ * Return: string that is encoded
  */
-int main(void)
+char *leet(char *c)
 {
-    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-    char *p;
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
 
-    p = leet(s);
-    printf("%s", p);
-    printf("%s", s);
-    return (0);
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			/*32 is the difference between lower case letters and apper case letters*/
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++;
+	}
+
+	return (cp);
 }
